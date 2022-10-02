a987e49e6abc8c6403a3714acf35a8c1248df18c
Cheegiti Mahesh <ramahesh024@gmail.com>
2022-10-02 12:07:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var MainImg = document.getElementById("MainImg");
+        var smallimg = document.getElementsByClassName("small-img")
+
+        smallimg[0].onclick = function(){
+            MainImg.src = smallimg[0].src;
+        }
+        smallimg[1].onclick = function(){
+            MainImg.src = smallimg[1].src;
+        }
+        smallimg[2].onclick = function(){
+            MainImg.src = smallimg[2].src;
+        }
+        smallimg[3].onclick = function(){
+            MainImg.src = smallimg[3].src;
+        }
+
+var form = document.getElementById('sheetdb-form');
+        form.addEventListener("submit", e => {
+          e.preventDefault();
+          fetch(form.action, {
+              method : "POST",
+              body: new FormData(document.getElementById("sheetdb-form")),
+          }).then(
+              response => response.json()
+          ).then((html) => {
+            // you can put any JS code
+            window.location.href = 'index.html';
+          });
+        });
